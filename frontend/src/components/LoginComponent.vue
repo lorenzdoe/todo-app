02d0a152191e43e7c62f76e35f4b20022b184b87
Lorenz Doellinger <if21b084@technikum-wien.at>
2023-10-24 00:15:25 +0200

    changes in signup and login

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -1,50 +1,116 @@
 <template>
-    <!-- To add: login image/logo -->
-    <h1>Login</h1>
-    <div>
-        <input type="text" placeholder="username" v-model="username">
-        <br>
-        <input type="password" placeholder="Enter password" v-model="password">
-        <br>
-        <button @click="login">Login</button>
-        <br><br>
-        <a href="/signup" style="color: antiquewhite; ">Create an account</a>
+    <div class="login-container">
+        <div class="login-form">
+            <h1 class="login-title">Login</h1>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" v-model="username" placeholder="Enter your username" />
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" v-model="password" placeholder="Enter your password" />
+            </div>
+            <button class="login-button" @click="login">Login</button>
+            <div class="signup-link">
+                <span>Don't have an account?</span>
+                <router-link to="/signup">Sign up</router-link>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import {login} from '@/api';
-export default{
-    name: 'LoginComponent',
-    data()
-    {
+import { login } from "@/api";
+export default {
+    name: "LoginComponent",
+    data() {
         return {
-            username: '',
-            password: ''
+            username: "",
+            password: "",
         };
     },
-    methods:{
-        async login()
-        {
-            if(this.username == '' || this.password == '')
-            {
-                alert('Please enter username and password');
+    methods: {
+        async login() {
+            if (this.username == "" || this.password == "") {
+                alert("Please enter username and password");
                 return;
             }
             let result = await login(this.username, this.password);
 
-            if(result.status == 200)
-            {
-                localStorage.setItem('token', result.data.token);
-                localStorage.setItem('username', this.username);
-                this.$router.push({name: 'TodoList'});
-            }
-            
-            else
-            {
+            if (result.status == 200) {
+                localStorage.setItem("token", result.data.token);
+                localStorage.setItem("username", this.username);
+                this.$router.push({ name: "TodoList" });
+            } else {
                 alert(result.data.errors);
             }
-        }
+        },
     },
 };
 </script>
+
+<style scoped>
+.login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.login-form {
+    padding: 30px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    max-width: 400px;
+    width: 100%;
+}
+
+.login-title {
+    font-size: 2rem;
+    margin-bottom: 20px;
+    text-align: center;
+}
+
+.form-group {
+    margin-bottom: 20px;
+}
+
+label {
+    display: block;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 1rem;
+}
+
+.login-button {
+    background-color: #007bff;
+    border: none;
+    border-radius: 5px;
+    color: #fff;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 10px;
+    width: 100%;
+}
+
+.login-button:hover {
+    background-color: #0069d9;
+}
+
+.signup-link {
+    margin-top: 20px;
+    text-align: center;
+}
+
+.signup-link span {
+    margin-right: 5px;
+}
+</style>
--- frontend/src/components/SignupComponent.vue
+++ frontend/src/components/SignupComponent.vue
@@ -1,14 +1,23 @@
 <template>
-    <!-- To add: login image/logo -->
-    <h1 id="inputText"></h1>
-    <br><br>
-    <div id="div" class="divStyle">
-        <input id="input1" type="text" placeholder="username" v-model="username">
-        <input id="input2" type="password" placeholder="Enter password" v-model="password">
-
-        <button id="button" @click="signup"></button>
-        <br>
-        <a href="/login" style="color: antiquewhite; ">Already have an account?</a>
+    <div class="signup-container">
+        <div class="signup-form">
+            <h1 class="signup-title">{{ title }}</h1>
+            <form>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input id="username" type="text" placeholder="Enter your username" v-model="username" />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" placeholder="Enter your password" v-model="password" />
+                </div>
+                <button class="signup-button" @click="signup">Sign Up</button>
+            </form>
+            <div class="login-link">
+                <span>Already have an account?</span>
+                <router-link to="/login">Log in</router-link>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -21,7 +30,8 @@ export default {
     data() {
         return {
             username: '',
-            password: ''
+            password: '',
+            title: '',
         };
     },
     methods: {
@@ -34,51 +44,108 @@ export default {
             let result = await signup(this.username, this.password, group);
             if (result.status == 201) {
                 this.$router.push({ name: 'Login' });
-            }
-            else if (result.status == 400) {
-                if (result.data.errors.msg != undefined)
-                    alert(result.data.errors);
+            } else if (result.status == 400) {
+                if (result.data.errors.msg != undefined) alert(result.data.errors);
                 else
-                    alert(result.data.errors[0].msg + ': ' + result.data.errors[0].param);
-            }
-            else {
+                    alert(
+                        result.data.errors[0].msg + ': ' + result.data.errors[0].param
+                    );
+            } else {
                 alert('Something went wrong');
             }
         },
-        setGroupA(){
-        localStorage.setItem('group', 'A');
-        document.getElementById('inputText').textContent = 'Want to be part of our evergrowing family?';
-        document.getElementById('input1').className = 'inputStyle';
-        document.getElementById('input2').className = 'inputStyle';
-        document.getElementById('button').className = 'buttonStyle';
-        document.getElementById('button').textContent = 'Join now!';
+        setGroupA() {
+            localStorage.setItem('group', 'A');
+            this.title = 'Want to be part of our evergrowing family?';
         },
-        setGroupB(){
+        setGroupB() {
             localStorage.setItem('group', 'B');
-            document.getElementById('inputText').textContent = 'Create account:';
-            document.getElementById('button').textContent = 'Sign-up!';
-        }
+            this.title = 'Create account:';
+        },
     },
     mounted() {
-        if(localStorage.group == undefined) {
+        if (localStorage.group == undefined) {
             if (Math.random() <= 0.5) {
                 group = 'A';
                 this.setGroupA();
-            }
-            else {
+            } else {
                 group = 'B';
                 this.setGroupB();
             }
-        }   
-        else {
+        } else {
             group = localStorage.group;
             if (group == 'A') {
                 this.setGroupA();
-            }
-            else {
+            } else {
                 this.setGroupB();
             }
         }
-    }
+    },
 };
 </script>
+
+<style scoped>
+.signup-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.signup-form {
+    padding: 30px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    max-width: 400px;
+    width: 100%;
+}
+
+.signup-title {
+    font-size: 2rem;
+    margin-bottom: 20px;
+    text-align: center;
+}
+
+.form-group {
+    margin-bottom: 20px;
+}
+
+label {
+    display: block;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 1rem;
+}
+
+.signup-button {
+    background-color: #007bff;
+    border: none;
+    border-radius: 5px;
+    color: #fff;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 10px;
+    width: 100%;
+}
+
+.signup-button:hover {
+    background-color: #0069d9;
+}
+
+.login-link {
+    margin-top: 20px;
+    text-align: center;
+}
+
+.login-link span {
+    margin-right: 5px;
+}
+</style>
